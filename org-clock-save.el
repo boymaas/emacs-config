725899ce0950b7e6ad03b00c7617a2b9745ae233
KXCG Maas <boy.maas@gmail.com>
2009-09-06 21:40:53 +0200

    Updated config. Removed clojure modes from manual install to handled by elpa

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,3 +1,3 @@
-;; org-persist.el - eve at <2009-09-06 Sun 19:41>
+;; org-persist.el - eve at <2009-09-06 Sun 21:32>
 (setq resume-clock '("/home/m4nic/Planning/casino.gtd" . 24301))
-(setq stored-clock-history '(("/home/m4nic/Planning/gtd.gtd" . 2832) ("/home/m4nic/Planning/gtd.gtd" . 11718) ("/home/m4nic/Planning/casino.gtd" . 24137)))
+(setq stored-clock-history '(  ("/home/m4nic/Planning/casino.gtd" . 24137)))
