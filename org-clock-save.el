f42f5677069827ee2c9d5b764cd271c43640e529
Boy Maas <m4nic@eve.(none)>
2009-10-05 22:00:03 +0200

    Mon Oct  5 22:00:03 CEST 2009

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,1 @@
-;; org-persist.el - eve at <2009-10-03 Sat 04:00>
-(setq stored-clock-history '(("/home/m4nic/Planning/innovita/casino.gtd" . 28377) ("/home/m4nic/Planning/innovita/casino.gtd" . 25602) ("/home/m4nic/Planning/innovita/casino.gtd" . 29006)))
+;; org-persist.el - eve at <2009-10-05 Mon 21:57>
